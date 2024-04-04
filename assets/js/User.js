88b128fc284ca7cc2755dfa4335643a24734e118
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-04-04 09:29:43 +0700

    getUsers class User

---
+++ assets/js/User.js
@@ -0,0 +1,19 @@
+class User {
+  constructor() {
+    this._users = null;
+  }
+
+  getUser() {
+    if ((this._users = null)) {
+      try {
+        const storedUsers = localStorage.getItem("users");
+
+        this._users = storedUsers ? JSON.parse(storedUsers) : [];
+      } catch (error) {
+        return (this._users = []);
+      }
+    }
+
+    return this._users;
+  }
+}
