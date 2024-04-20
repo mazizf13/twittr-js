1c940247fccfb922c0080be23310ccc7656cc4b8
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-04-20 20:20:51 +0700

    feature: save data user

--- assets/js/User.js
+++ assets/js/User.js
@@ -62,6 +62,17 @@ class User {
 
     const users = this.getUsers();
     users.push(newUser);
+
+    try {
+      localStorage.setItem("users", JSON.stringify(users));
+      return {
+        success: true,
+      };
+    } catch (error) {
+      return {
+        success: false,
+      };
+    }
   }
 
   userSignIn() {}
